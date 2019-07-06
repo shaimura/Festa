d16c184417a11815c1d9f4e18ad5882a553dfd15
shaimura <daken240515@gmail.com>
2019-07-06 06:08:14 +0000

    start search

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def Festival.search(search)
+		if search
+			where(['name LIKE ?', "%#{search}%"])
+		else
+			all
+		end
+	end
+
 end
